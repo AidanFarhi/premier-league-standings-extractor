cec3828a237b4e3afa10a9dc8e87195174f7bbcf
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-02-05 17:24:07 -0500

    updated script and enabled github actions

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 import os
 import boto3
 import requests
+import json
 from dotenv import load_dotenv
 from datetime import date
 load_dotenv()
 
-
-# goals_df = list(filter(lambda t: 'G' in t.columns, tables))[0]
-# assists_df = list(filter(lambda t: 'A' in t.columns, tables))[0]
-# goals_buffer = goals_df.to_csv(index=False).encode()
-# assists_buffer = assists_df.to_csv(index=False).encode()
-# client = boto3.client(
-#     's3', 
-#     endpoint_url=os.getenv('ENDPOINT_URL'),
-#     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-#     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-# )
-# client.put_object(
-#     Bucket=os.getenv('BUCKET_NAME'), 
-#     Key=f'premier_league/player/top_scorers_{date.today()}.csv',
-#     Body=goals_buffer
-# )
-# client.put_object(
-#     Bucket=os.getenv('BUCKET_NAME'), 
-#     Key=f'premier_league/player/top_assisters_{date.today()}.csv', 
-#     Body=assists_buffer
-# )
+url = 'https://api-football-v1.p.rapidapi.com/v3/standings'
+querystring = {'season':'2022','league':'39'}
+headers = {
+	'X-RapidAPI-Key': os.getenv('RAPID_API_KEY'),
+	'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+}
+response = requests.request('GET', url, headers=headers, params=querystring)
+data = response.json()
+json_string_from_data = json.dumps(data)
+client = boto3.client(
+    's3', 
+    endpoint_url=os.getenv('ENDPOINT_URL'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+client.put_object(
+    Bucket=os.getenv('BUCKET_NAME'), 
+    Key=f'premier_league/standings/{date.today()}.json',
+    Body=json_string_from_data
+)
+client.put_object(
+    Bucket=os.getenv('BUCKET_NAME'), 
+    Key=f'premier_league/standings/latest.json',
+    Body=json_string_from_data
+)
